10f2ed69e9564cb4434d0d7bdde85bde503e1010
Felipe Martini <felipecmartini@gmail.com>
2022-11-28 22:57:10 -0300

    icon spinning added

--- main.js
+++ main.js
@@ -14,15 +14,19 @@ questionButton.forEach((item) => {
 
 function checkActive(item, answer, answer__active) {
     const icon_item = item.querySelector(".icon");
-
+    // const question_item = item.querySelector(".question__box");
     const icons = document.querySelectorAll(".icon");
 
     if (answer !== answer__active) {
         answer.classList.add("active");
+        icon_item.classList.add("iconActive");
+        // question_item.classList.add("questionActive");
     }
 
     if (answer__active){
-        answer__active.classList.remove("active")
+        answer__active.classList.remove("active");
+        icon_item.classList.remove("iconActive");
+        // question_item.classList.remove("questionActive");
     }
 }
 
