bb908d25f584c4111b5e23bc3325c6560f7f2dfc
Felipe Martini <felipecmartini@gmail.com>
2022-11-28 23:34:32 -0300

    active class error fixed

--- main.js
+++ main.js
@@ -14,20 +14,35 @@ questionButton.forEach((item) => {
 
 function checkActive(item, answer, answer__active) {
     const icon_item = item.querySelector(".icon");
-    // const question_item = item.querySelector(".question__box");
-    const icons = document.querySelectorAll(".icon");
+    const question_item = item.querySelector(".question__box");
 
-    if (answer !== answer__active) {
-        answer.classList.add("active");
-        icon_item.classList.add("iconActive");
-        // question_item.classList.add("questionActive");
-    }
+    const icons = document.querySelectorAll(".icon");
+    const questions = document.querySelectorAll(".question__box");
+
+    icons.forEach((item) => (item.classList.remove("iconActive")))
+    questions.forEach((item) => (item.classList.remove("questionActive")))
+
+
+    // clearActive(question_item, icon_item);
+
+
 
     if (answer__active){
         answer__active.classList.remove("active");
         icon_item.classList.remove("iconActive");
-        // question_item.classList.remove("questionActive");
+        question_item.classList.remove("questionActive");
     }
+
+    if (answer !== answer__active) {
+        answer.classList.add("active");
+        icon_item.classList.add("iconActive");
+        question_item.classList.add("questionActive");
+    } 
 }
+
+// function clearActive (question, icon){
+//     question.classList.remove("questionActive");
+//     icon.classList.remove("iconAtive")
+// }
 
 
