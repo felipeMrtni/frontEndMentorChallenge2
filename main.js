fe8a85462da2542d1b4c80e84162d80f0e3ccc14
Felipe Martini <felipecmartini@gmail.com>
2022-11-29 09:38:20 -0300

    active stayles changes in main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ questionButton.forEach((item) => {
 
     question__box__item.addEventListener("click", () => {
         const answer__box__item = item.querySelector(".answer__box"); 
-
         const answer__box__active = document.querySelector(".active")
 
         checkActive(item, answer__box__item, answer__box__active)
@@ -19,30 +18,29 @@ function checkActive(item, answer, answer__active) {
     const icons = document.querySelectorAll(".icon");
     const questions = document.querySelectorAll(".question__box");
 
-    icons.forEach((item) => (item.classList.remove("iconActive")))
-    questions.forEach((item) => (item.classList.remove("questionActive")))
-
-
-    // clearActive(question_item, icon_item);
-
-
+    icons.forEach((item) => (item.style.rotate = "0deg"))
+    // icons.forEach((item) => (item.classList.remove("iconActive")))
+    questions.forEach((item) => (item.style.fontWeight = "400"))
+    // questions.forEach((item) => (item.classList.remove("questionActive")))
 
     if (answer__active){
         answer__active.classList.remove("active");
-        icon_item.classList.remove("iconActive");
-        question_item.classList.remove("questionActive");
+        // icon_item.classList.remove("iconActive");
+        // question_item.classList.remove("questionActive");
+        
     }
 
     if (answer !== answer__active) {
         answer.classList.add("active");
-        icon_item.classList.add("iconActive");
-        question_item.classList.add("questionActive");
+        // icon_item.classList.add("iconActive");
+        // question_item.classList.add("questionActive");
+        icon_item.style.rotate = "180deg";
+        question_item.style.fontWeight = "700";
     } 
 }
 
-// function clearActive (question, icon){
-//     question.classList.remove("questionActive");
-//     icon.classList.remove("iconAtive")
-// }
+
+
+
 
 
